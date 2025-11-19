1cf2580c290f7bd46c0267eabc2cbad4ed5c3da0
quietbytesilence <quietbytequietbyte@gmail.com>
2025-11-19 14:57:50 -0300

    novidades

---
+++ cursos.js
@@ -0,0 +1,42 @@
+/*categorias
+programacao
+dados
+computacao
+math
+*/
+
+
+window.cards = {
+    "curso1": {
+        curso: "Alquimia de Dados",
+        descricao: "Domine a transformação de dados brutos em conhecimento valioso através de técnicas avançadas de análise e visualização.",
+        categoria: "dados",
+        duracao: "80 horas",
+        nivel: "Intermediário"
+    },
+    "curso2": {
+        curso: "Feitiços Web",
+        descricao: "Aprenda as artes obscuras do desenvolvimento front-end e back-end para criar aplicações web poderosas.",
+        categoria: "web",
+        duracao: "120 horas",
+        nivel: "Avançado"
+    },"curso3": {
+        curso: "Feitiços Web",
+        descricao: "Aprenda as artes obscuras do desenvolvimento front-end e back-end para criar aplicações web poderosas.",
+        categoria: "web",
+        duracao: "120 horas",
+        nivel: "Avançado"
+    },
+
+
+
+
+
+    "curso2": {
+        curso: "Feitiços Web",
+        descricao: "Aprenda as artes obscuras do desenvolvimento front-end e back-end para criar aplicações web poderosas.",
+        categoria: "web",
+        duracao: "120 horas",
+        nivel: "Avançado"
+    },
+};
