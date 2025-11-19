5b4bc540e68823c824cc704f8db8266a54d9cb25
quietbytesilence <quietbytequietbyte@gmail.com>
2025-11-19 14:15:39 -0300

    site

---
+++ certificados.js
@@ -0,0 +1,14 @@
+window.certificados = {
+  "A1B2C3D4E5": {
+    nome: "Gustavo Cícero",
+    curso: "Matemática Aplicada",
+    carga: 40,
+    data: "15/10/2020"
+  },
+  "484ac9381b925a55f201e3fcc035ced2": {
+    nome: "Maria Silva",
+    curso: "Data Science",
+    carga: 60,
+    data: "15/11/2025"
+  }
+};
---
+++ dados.js
@@ -0,0 +1,15 @@
+window.cards = {
+  "Redes_Basico" : {
+    curso: "Rede de Computadores - Básico",
+    descricao: "#RCB - Seja bem-vindo ao curso de Rede de Computadores - Básico, nele você aprenderá o essencial sobre Rede de Computadores"
+
+  },
+  "FundMath" : {
+    curso: "Fundamentos da Matemática Aplicada em Ciência da Computação",
+    descricao: "Aprenda os Fundamentos da Matemática, desde os bits aos microprocessadores. Venha embarcar nessa jornada rumo ao entendimento fundamental da computação"
+  }
+
+
+  
+};
+
